26ac671a35e6a783fd5ef818548d44a5c312fc7f
Abu Sayem <78730784+BinaryBeast007@users.noreply.github.com>
2021-06-20 22:48:33 +0600

    Added Infix To Postfix With Parenthesis

---
+++ C++/Data-Structures/Stack/infix_to_postfix_with_parenthesis.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isOperator(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch== '^');
+} 
+bool isOperand(char ch) {
+	return ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
+}
+int precedence(char ch) {
+    int prec = 0;
+    switch(ch) {
+        case '+':
+        case '-':
+            prec = 1;
+            break;
+        case '*':
+        case '/':
+            prec = 2;
+            break;
+        case '^':
+            prec = 3;
+            break;
+        default:
+            prec = -1;
+            break;
+    }
+    return prec;
+}
+string infixToPostfix(string exp) {
+    string postfix = "";
+    stack<int> st;
+    for(int i=0; i<exp.length(); i++) {
+        if(isOperand(exp[i])) {
+            postfix += exp[i];
+        }else if(exp[i] == '(') {
+            st.push(exp[i]);
+        }else if(exp[i] == ')') {
+            while(!st.empty() && st.top() != '(') {
+                postfix += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+        else if(isOperator(exp[i])) {
+            while(!st.empty() && precedence(exp[i]) <= precedence(st.top())) {
+                postfix += st.top();
+                st.pop();
+            }
+            st.push(exp[i]);
+        }
+    }
+    while(!st.empty()) {
+        postfix += st.top();
+        st.pop();
+    }
+    return postfix;
+}
+
+int main() {
+    string infix = "A+(B-C^D/E*F+(G-H)-I)*K^L";
+    string posfix = infixToPostfix(infix);
+
+    cout << "infix\t: " << infix << "\n";
+    cout << "postfix\t: " << posfix << "\n";
+
+    return 0;
+}
