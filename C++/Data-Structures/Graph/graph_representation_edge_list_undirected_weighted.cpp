50a8003b4b543c73daf293e28a3f590778e3e13b
Abu Sayem <78730784+BinaryBeast007@users.noreply.github.com>
2021-07-29 11:37:45 +0600

    Added graph representation edge list

---
+++ C++/Data-Structures/Graph/graph_representation_edge_list_undirected_weighted.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+struct Edge {
+    int startVertex;
+    int endVertex;
+    int weight;
+};
+
+int main() {
+    int v, e;
+    cout << "number of nodes/vertices : ";
+    cin >> v;
+    cout << "number of edges : ";
+    cin >> e;
+
+    int vertex_list[v];
+    Edge edge_list[e];
+
+    cout << "input the nodes/vertices :\n";
+    for(int i=0; i<v; i++) {
+        cin >> vertex_list[i];
+    }
+    cout << "input the edges :\n";
+    for(int i=0; i<e; i++) {
+        Edge edge;
+        cout << "start vertex : ";
+        cin >> edge.startVertex;
+        cout << "end vertex : ";
+        cin >> edge.endVertex;
+        cout << "weight : ";
+        cin >> edge.weight;
+        edge_list[i] = edge;
+    }
+    cout << "the nodes/vertices :\n";
+    for(int i=0; i<v; i++) {
+        cout << vertex_list[i] << " ";
+    }
+    cout << "\nthe edges :\n";
+    for(int i=0; i<e; i++) {
+        cout << edge_list[i].startVertex << " " << edge_list[i].endVertex << " " << edge_list[i].weight << '\n';
+    }
+
+    return 0;
+}
